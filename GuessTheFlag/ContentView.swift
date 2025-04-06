bf376bfbd429d52aba5cf8fc7a0e62ce673847fe
zoli0829 <zoli0829@gmail.com>
2025-04-06 09:27:24 +0100

    day 20

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello world!")
         }
-        .padding()
+    }
+    
+    func executeDelete() {
+        print("Now deleting...")
     }
 }
 
